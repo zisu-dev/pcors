17c8488e64386212ed00315f04cb791d1b7a6ad8
ZhangZisu <i@zzs1.cn>
2020-12-31 17:49:49 +0800

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,37 @@
-console.log('Hello world!')
+import yargs from 'yargs'
+import { createProxyMiddleware } from 'http-proxy-middleware'
+import express from 'express'
+
+yargs.command(
+  '$0 <port> <target>',
+  'Run CORS Proxy',
+  (yargs) =>
+    yargs
+      .positional('port', {
+        describe: 'Local listen port',
+        type: 'number',
+        demandOption: true
+      })
+      .positional('target', {
+        describe: 'Target URL',
+        type: 'string',
+        demandOption: true
+      }),
+  (args) => {
+    const app = express()
+    app.use(
+      createProxyMiddleware({
+        target: args.target,
+        changeOrigin: true,
+        onProxyReq(proxyReq, req, res) {
+          console.log(`[${req.method}] ${req.url}`)
+        },
+        onProxyRes(proxyRes, req, res) {
+          proxyRes.headers['Access-Control-Allow-Origin'] = '*'
+        }
+      })
+    )
+    app.listen(args.port)
+    console.log('Listening on port ' + args.port)
+  }
+).argv
